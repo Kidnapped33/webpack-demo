cb3daff7c62c1e269239cdb7947e5038cc3a6ba1
Ashine <404770266@qq.com>
2020-06-25 16:44:03 +0800

    uptate

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
@@ -5,5 +6,17 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: '[name].[chunkhash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '33',//html的抬头
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/, //$的意思是以css结尾，\.的意思是转译
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }
 };
